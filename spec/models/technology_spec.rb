76279671228617313248b87ad92f0a262d632b5d
rrgayhart <rrgayhart@gmail.com>
2016-02-15 16:16:44 -0700

    Set both sides of has and belongs to

--- spec/models/technology_spec.rb
+++ spec/models/technology_spec.rb
@@ -11,6 +11,10 @@ describe Technology do
     it { expect(instance).to validate_presence_of(:name) }
   end
 
+  describe "Associations" do
+    it { expect(instance).to have_and_belong_to_many(:jobs) }
+  end
+
   describe "Callbacks" do
     it 'downcases itself before saving' do
       object = create(:technology, name: 'Ruby')
