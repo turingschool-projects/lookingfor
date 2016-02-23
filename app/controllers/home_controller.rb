7c13eaeb75d9fe4c4403181589a40c7efa24ddcb
Michael Wong <michael.brian.wong@gmail.com>
2016-02-22 20:31:31 -0700

    Lists jobs with Posted Date and Company Name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @job_count = Job.count
+    @jobs = Job.all
     @company_count = Company.count
     @tech_names = Technology.pluck(:name)
   end
