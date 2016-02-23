b8206f71622f775c1ae3c830cee1892949e53d15
Michael Wong <michael.brian.wong@gmail.com>
2016-02-23 10:43:33 -0700

    Sorts job postings by date in descending order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @job_count = Job.count
-    @jobs = Job.paginate(page: params[:page])
+    @jobs = Job.paginate(page: params[:page]).order('posted_date DESC')
     @company_count = Company.count
     @tech_names = Technology.pluck(:name)
   end
