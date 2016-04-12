6fb61750aa10b479206e1707b0d647d1e98143d0
Edgar Duran <edgar.eduran@gmail.com>
2016-04-12 11:28:52 -0600

    add faraday gem and set up connection service

--- app/services/angel_list_service.rb
+++ app/services/angel_list_service.rb
@@ -1,5 +1,12 @@
 class AngelListService
+  attr_reader :connection
 
-
+  def initialize
+    @connection = Faraday.new(:url => 'https://angel.co/api') do |faraday|
+      faraday.request :url_encoded
+      faraday.response :logger
+      faraday.adapter Faraday.default_adapter
+    end
+  end
 
 end
