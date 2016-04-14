4471241678d6281b4ea36bf222095608e82a4517
David Stinnette <dastinnette@gmail.com>
2016-04-14 09:42:29 -0600

    Issue #23: Created Company Show page
    Issue #23: Created Company Show page
    
    Issue #23: Created Company Show page
    
    Issue #23: Created Company Show page
    
    Created companies controller, added show route
    
    Built company view skeleton
    
    Added panels, pulling company job data into view
    
    Company tech stack printing to show page
    
    Updated header to include link back to home page
    
    Add link to jobs#show from home page
    
    Successfully implemented pagination, jobs listed most recent at top
    
    Moved technologies in view
    
    Created companies controller, added show route
    
    Built company view skeleton
    
    Add link to jobs#show from home page
    
    Update home_controller.rb
    
    Update show.html.erb
    
    Removed two <br> and brought in @job.each instead of @company.jobs.each
    
    Update Gemfile.lock
    
    Added testing for companies controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  resources :companies, only: [:show]
 
   namespace :api do
     namespace :v1 do
