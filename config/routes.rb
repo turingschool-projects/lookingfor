5fc357bc67b8d813a9759eeca8647991823deffd
David Stinnette <dastinnette@gmail.com>
2016-04-19 08:37:44 -0600

    Fixed merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  resources :companies, only: [:show]
 
   resources :jobs, only: [:show]
 
