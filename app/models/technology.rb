dcc7c85249909a592b5ed0cd1b1ef90cba4bbbea
Jared Roth <jared.roth@gmail.com>
2016-08-03 08:47:19 -0600

    Add exploratory method for counting jobs by technology

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -2,4 +2,8 @@ class Technology < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_and_belongs_to_many :jobs
   before_save { |tech| tech.name = tech.name.downcase }
+
+  def self.test_analysis(tech)
+    find_by(name: tech).jobs.count
+  end
 end
