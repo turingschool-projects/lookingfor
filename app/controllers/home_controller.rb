60841c7a4c8fdc22972f6dcfb04fc16a882141f5
Jason Hanna <jason.hanna01@gmail.com>
2016-11-04 10:20:07 -0600

    add mailer functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,16 @@ class HomeController < ApplicationController
     @company_count = Company.count
     @tech_names = Technology.pluck(:name)
   end
+
+  def send_jobs_email
+    # Create the user from params
+    @user = User.new(params[:user])
+    if @user.save
+      # Deliver the signup email
+      UserNotifier.send_signup_email(@user).deliver
+      redirect_to(@user, :notice => 'User created')
+    else
+      render :action => 'new'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :jobs, only: [:show]
   resources :companies, only: [:show]
 
+  post '/jobs_email' to: "home#send_jobs_email"
+
   namespace :api do
     namespace :v1 do
       resources :jobs, only: [:index, :show]
