cdafd114046b0c44e3161e241b6f3b4cf79c0a12
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-22 14:53:41 -0700

    adjust test

--- app/services/job_creator.rb
+++ app/services/job_creator.rb
@@ -17,18 +17,28 @@ class JobCreator
     end
 
     def self.process_payload(job)
-      response = Faraday.get("https://turingmonocle-staging.herokuapp.com/api/v1/companies/find?name=#{job['company']}")
+
+      conn = Faraday.new('https://localhost:3001/api/v1/companies', { ssl: { verify: false } })
+      # response = Faraday.get("https://turingmonocle-staging.herokuapp.com/api/v1/companies/find?name=#{job['company']}")
+      response = conn.get("/find?name=#{job['company']}")
 
       if response.status == 404
         company_data = { company: {name: job['company']}, token: 'TurMonLook4'}
-        post_response = Faraday.post("https://turingmonocle-staging.herokuapp.com/api/v1/companies", company_data)
+        # post_response = Faraday.post("https://turingmonocle-staging.herokuapp.com/api/v1/companies", company_data)
+        post_response = conn.post do |req|
+          req.params = company_data
+        end
+
+        # if post_response.status == 500
+        #   require 'pry'; binding.pry
+        # end
         monocle_response = JSON.parse(post_response.body)
 
         company = Company.find_or_create_by!(name: monocle_response['name'])
         company.monocle_id = monocle_response['id']
         company.save
-        company.jobs.create!(title: job['title'], description: job['description'], url: job['url'])
-
+        job = company.jobs.create!(title: job['title'], description: job['description'], url: job['url'])
+        puts "created/found #{company.name} with created #{job.title}"
       else
         parsed_response = JSON.parse(response.body)
 
@@ -36,6 +46,7 @@ class JobCreator
         company.monocle_id = parsed_response['company_id']
         company.save!
         company.jobs.create!(title: job['title'], description: job['description'], url: job['url'])
+        puts "created #{job.title}"
       end
     end
 
