52fc2c87b5c23cafd32f5ea80f6587fe89e01dc4
Lee Saville <l.w.saville@gmail.com>
2017-02-25 18:24:08 -0700

    Standardize symblization of names. Fix nesting errors

--- app/services/job_creator.rb
+++ app/services/job_creator.rb
@@ -13,7 +13,7 @@ class JobCreator
 
     queue.subscribe do |delivery_info, metadata, payload|
       puts "I hit the queue!"
-      data = JSON.parse(payload)
+      data = parse(payload)
       formatted_data = find_location_and_format_data(data)
       process_payload(formatted_data)
     end
@@ -23,13 +23,12 @@ class JobCreator
   end
 
   def self.process_payload(formatted_data)
-    binding.pry
     conn = Faraday.new("https://turingmonocle-staging.herokuapp.com/api/v1/companies", { ssl: { verify: false } })
     # response = Faraday.get("https://turingmonocle-staging.herokuapp.com/api/v1/companies/find?name=#{job['company']}")
-    response = conn.get("/find?name=#{formatted_data['company']['name']}")
+    response = conn.get("/find?name=#{formatted_data[:company][:name]}")
 
     if response.status == 404
-      company_data = { company: {name: job['company']}, token: 'TurMonLook4'}
+      company_data = { company: {name: formatted_data[:company][:name]}, token: 'TurMonLook4'}
       # post_response = Faraday.post("https://turingmonocle-staging.herokuapp.com/api/v1/companies", company_data)
       post_response = conn.post do |req|
         req.params = company_data
@@ -38,20 +37,20 @@ class JobCreator
       # if post_response.status == 500
       #   require 'pry'; binding.pry
       # end
-      monocle_response = JSON.parse(post_response.body)
+      monocle_response = parse(post_response.body)
 
-      company = Company.find_or_create_by!(name: monocle_response['name'])
-      company.monocle_id = monocle_response['id']
+      company = Company.find_or_create_by!(name: monocle_response[:name])
+      company.monocle_id = monocle_response[:id]
       company.save
-      job = company.jobs.create!(title: job['title'], description: job['description'], url: job['url'])
+      job = company.jobs.create!(title: formatted_data[:job][:title], description: formatted_data[:job][:description], url: formatted_data[:job][:url])
       puts "created/found #{company.name} with created #{job.title}"
     else
-      parsed_response = JSON.parse(response.body)
+      parsed_response = parse(response.body)
 
-      company = Company.find_or_create_by!(name: job['company'])
-      company.monocle_id = parsed_response['company_id']
+      company = Company.find_or_create_by!(name: formatted_data[:company][:name])
+      company.monocle_id = parsed_response[:company_id]
       company.save!
-      company.jobs.create!(title: job['title'], description: job['description'], url: job['url'])
+      company.jobs.create!(title: formatted_data[:job][:title], description: formatted_data[:job][:description], url: formatted_data[:job][:url])
       puts "created #{job.title}"
     end
   end
@@ -60,8 +59,8 @@ class JobCreator
     #Hit google and get location
     #successful hit
     #unscuccessful hit
-    company = data["company"]['name']
-    location = data['location']['name'] || "Denver"
+    company = data[:company][:name]
+    location = data[:location][:name] || "Denver"
     #something here to check for location name and if not, default to denver because builtin is the only one that doesn't have it
     conn = Faraday.new("https://maps.googleapis.com/maps/api/place/textsearch/json")
 
@@ -71,13 +70,12 @@ class JobCreator
       req.params['location'] = location
       req.params['radius'] = '500'
     end
-    binding.pry
-    parsed_response = JSON.parse(response.body)
+    parsed_response = parse(response.body)
 
-    if parsed_response['status'] == 'ZERO_RESULTS'
+    if parsed_response[:status] == 'ZERO_RESULTS'
       format_without_address(data)
     else
-      address = JSON.parse(response.body)["results"].first["formatted_address"]
+      address = parse(response.body)[:results].first[:formatted_address]
       format_with_address(data, address)
     end
   end
@@ -85,41 +83,53 @@ class JobCreator
   def self.format_with_address(data, address)
     address = address.split(',')
     blah = { job: {
-        title: data['job']['title'],
-        url: data['job']['url'],
-        raw_technologies: data['job']['raw_technologies'],
-        description: data['job']['description'],
-        remote: data['job']['remote'],
-        posted_date: data['job']['published']
+        title: data[:job][:title],
+        url: data[:job][:url],
+        raw_technologies: data[:job][:raw_technologies],
+        description: data[:job][:description],
+        remote: data[:job][:remote],
+        posted_date: data[:job][:published]
       },
       company: {
-        name: data['company']['name']
+        name: data[:company][:name]
       },
-      location: {
-        street_address: address[0],
-        city: address[1].strip,
-        state: address[2].split.first.strip,
-        zip_code: address[2].split[1].strip
-      }
+      location: blah(address)
     }
   end
 
   def self.format_without_address(data)
     blah = { job: {
-        title: data['job']['title'],
-        url: data['job']['url'],
-        raw_technologies: data['job']['raw_technologies'],
-        description: data['job']['description'],
-        remote: data['job']['remote'],
-        posted_date: data['job']['published']
+        title: data[:job][:title],
+        url: data[:job][:url],
+        raw_technologies: data[:job][:raw_technologies],
+        description: data[:job][:description],
+        remote: data[:job][:remote],
+        posted_date: data[:job][:published]
       },
       company: {
-        name: data['company']['name']
+        name: data[:company][:name]
       },
       location: {
         name: nil
       }
     }
+  end
+
+  def self.blah(address)
+    if address[2].split[1]
+      {
+        street_address: address[0],
+        city: address[1].strip,
+        state: address[2].split.first.strip,
+        zip_code: address[2].split[1].strip
+      }
+    else
+      { name: nil }
+    end
+  end
+
+  def self.parse(data)
+    JSON.parse(data, symbolize_names: true)
   end
 
 end
